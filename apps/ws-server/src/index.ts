01c36bafbf591aa4878424493e76560d675c7bc1
krovi <buildsbykrishna@gmail.com>
2026-02-09 10:45:36 +0530

    Add JWT authentication for WebSocket connections

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,10 +1,27 @@
 import {WebSocketServer} from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const wss = new WebSocketServer({port: 8080});
 
 
-wss.on("connection",(socket)=>{
+wss.on("connection",(socket,request)=>{
     console.log("Client connected");
+    const url = request.url;
+    const querryParams = new URLSearchParams(url?.split("?")[1]);
+    const token = querryParams.get("token");
+    if(!token){
+        socket.send("Unauthorized");
+        socket.close();
+        return;
+    }
+
+    const decoded = jwt.verify(token, "secretkey");
+   
+    if(!(decoded as JwtPayload).userId){
+        socket.send("Unauthorized");
+        socket.close();
+        return;
+    }
 
     socket.on("message",(msg)=>{
         socket.send(`Echo: ${msg.toString()}`);
